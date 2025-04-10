abc59f527c23af1f559dc5f8b152e35659f1d883
Sandy <1sandyandos@gmail.com>
2025-04-11 01:06:56 +0300

    intro and variables

---
+++ IntroToJS.js
@@ -0,0 +1,25 @@
+console.log("My First Line :') "); 
+console.log('My second line hh, im just calculating the length here'.length)
+console.log('Hi my name is '+ 'sAndY'.toUpperCase());
+console.log(Math.ceil(Math.random()))
+
+//variables here
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+//let variable
+let changeMe = true;
+changeMe = false
+console.log(changeMe)
+
+//we cannot change the const ok? ok
+const name = 'Sandy';
+//name = 'Kate'
+console.log('this will show an error if we logged name so i commented it')
+
+//easy are the below:
+var favoriteAnimal = 'samakeee';
+console.log('My favorite animal: ' + favoriteAnimal )
+
+
