9120ec9bad28bb1a04c49ef99fa33238fecf7b4d
Sandy <1sandyandos@gmail.com>
2025-04-13 23:28:08 +0300

    arrays

--- arrays.js
+++ arrays.js
@@ -9,3 +9,46 @@ let groceryList = ['bread', 'tomatoes', 'milk'];
 groceryList[1] ='avocados';
 
 //you cant update an element in a const variable
+
+
+
+//to add we use  push
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+
+itemTracker.push('item 3', 'item 4');
+
+console.log(itemTracker); 
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+
+
+//too delete, declare a variable at first then log the first variable
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+const removed = chores.pop();
+console.log(chores)
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+// shift removes the first item ('orange juice')
+groceryList.shift();
+console.log(groceryList); 
+// Output: ['bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains']
+
+// unshift adds 'popcorn' to the beginning
+groceryList.unshift('popcorn');
+console.log(groceryList); 
+// Output: ['popcorn', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains']
+
+// slice to get 'bananas', 'coffee beans', 'brown rice' — all in one line
+console.log(groceryList.slice(1, 4));
+// Output: ['bananas', 'coffee beans', 'brown rice']
+
+
+
+//nested arrays
+const nestedArr = [[1], [2, 3]];
+
+console.log(nestedArr[1]); // Output: [2, 3]
+
+
+
