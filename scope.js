b9b161679e3e901ffbafa3be03177f5f1d3ebecf
Sandy <1sandyandos@gmail.com>
2025-04-13 21:41:56 +0300

    scope and blocks

---
+++ scope.js
@@ -0,0 +1,10 @@
+//A block is the code found inside a set of curly braces {}
+//const variable declaring
+const city = 'New York City'
+//function
+function logCitySkyline(){
+    //let variable
+  let skyscraper = 'Empire State Building'
+  return 'The stars over the ' + skyscraper + ' in ' + city
+  console.log(logCitySkyline())
+};
