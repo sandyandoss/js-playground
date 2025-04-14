f470d8b40d20046efa347a75fc89788e142f5af3
Sandy <1sandyandos@gmail.com>
2025-04-14 15:07:51 +0300

    loops

---
+++ loops.js
@@ -0,0 +1,11 @@
+// imaging if we have 100 places  we cannot log each one alone
+var vacationSpots = ['Dolomites', 'Switzerland' , 'Thailand']
+console.log(vacationSpots[0])
+console.log(vacationSpots[1])
+console.log(vacationSpots[2])
+
+
+
+
+
+
