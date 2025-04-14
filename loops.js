5737c500f7ceefeda4685cfa115adfc78c07b48c
Sandy <1sandyandos@gmail.com>
2025-04-14 15:57:37 +0300

    loops

--- loops.js
+++ loops.js
@@ -51,6 +51,22 @@ while(currentCard != 'spade'){
   currentCard = cards[Math.floor(Math.random() * 4)];
 console.log(currentCard)
 }
+//functions as data 
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for(let i = 1; i <= 1000000; i++) {
+      if ( (2 + 2) != 4) {
+        console.log('Something has gone very wrong :( ');
+      }
+    }
+  };
+  
+  // Using const, declare a shorter-named variable, isTwoPlusTwo that will be easier to work with.
+  const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+  isTwoPlusTwo() 
+  //Hmmm, if we forgot the original name of our function. Is there a way we could figure it out?
+  console.log(isTwoPlusTwo.name)
+  
+  
 
 
 
