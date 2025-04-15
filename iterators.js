24dba9b4cca94a36df49b84aca75ce5311c49a16
Sandy <1sandyandos@gmail.com>
2025-04-15 15:58:56 +0300

    iterators

--- iterators.js
+++ iterators.js
@@ -39,3 +39,19 @@ console.log(longFavoriteWords)
 //const startsWithS = animals.findIndex(animal => animal.startsWith('s'));
 
 console.log(startsWithS); // Output: 5
+
+
+
+//.some
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(word => word.length < 6));
+
+// Use filter to create a new array
+const interestingWords = words.filter(word => word.length >5)
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+console.log(interestingWords.every(word => word.length > 5));
