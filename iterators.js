89cfdec1374b8bf199ab019ffa1280cbcceaa76c
Sandy <1sandyandos@gmail.com>
2025-04-15 20:16:04 +0300

    iterators

--- iterators.js
+++ iterators.js
@@ -55,3 +55,27 @@ const interestingWords = words.filter(word => word.length >5)
 
 // Make sure to uncomment the code below and fix the incorrect code before running it
 console.log(interestingWords.every(word => word.length > 5));
+
+//revision to all methods
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
