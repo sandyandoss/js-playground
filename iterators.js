ca25024d8f48ced3745ec0e87df002156784d922
Sandy <1sandyandos@gmail.com>
2025-04-15 15:19:28 +0300

    iterators

--- iterators.js
+++ iterators.js
@@ -7,3 +7,20 @@ fruits.forEach(fruit => {
 });
 
 
+//2- .map()
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+const secretMessage = animals.map(animal => {
+return animal[0]
+})
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(numbers =>{
+  return numbers / 100;
+})
+console.log(smallNumbers)
