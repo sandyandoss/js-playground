e8641814d6cbfe9071f8895666d84a4240920fb2
Sandy <1sandyandos@gmail.com>
2025-04-16 23:30:17 +0300

    objects

--- object.js
+++ object.js
@@ -28,3 +28,14 @@ const goat = {
     console.log('baaa');
   }
 };
+
+//this keyword
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo(){
+   return `I am ${this.model} and my current energy level is ${this.energyLevel}.  
+`
+  }
+};
+console.log(robot.provideInfo());
