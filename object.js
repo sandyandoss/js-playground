da1862076cb70447d3a35b5d6779ef52960627bc
Sandy <1sandyandos@gmail.com>
2025-04-16 23:18:59 +0300

    objects

---
+++ object.js
@@ -0,0 +1,24 @@
+let spaceship = {}; // spaceship is an empty object
+
+//spaceships with keys and values
+// Write your fasterShip object literal below
+let fasterShip = {
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel'
+  }
+
+//accessing properties
+/*
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  // Write your code below
+  const crewCount = spaceship.numCrew;
+  const planetArray = spaceship.flightPath; */
+  
+//advanced objects
