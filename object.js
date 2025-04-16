d59b5be50b3375cd9bd976019dc8218aaff19c18
Sandy <1sandyandos@gmail.com>
2025-04-16 23:36:21 +0300

    objects

--- object.js
+++ object.js
@@ -39,4 +39,12 @@ const robot = {
   }
 };
 console.log(robot.provideInfo());
-//
+//or we remove the word function and add =>
+  const robot = {
+    energyLevel: 100,
+    checkEnergy: function()  {
+      console.log(`Energy is currently at ${this.energyLevel}%.`)
+    }
+  }
+  
+  robot.checkEnergy();
