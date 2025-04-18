60b7661015c03f0d3971ef9ae1566fa962d2a026
Sandy <1sandyandos@gmail.com>
2025-04-18 23:38:13 +0300

    objects

--- object.js
+++ object.js
@@ -100,3 +100,17 @@ const robotFactory = (model, mobile) => {
     }
   };
 };
+
+
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  };
+};
+
+const tinCan = robotFactory('P-500' , true)
+tinCan.beep()
